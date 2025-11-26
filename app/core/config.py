e86578e3d9cba45980f62fb40f623394257983c4
Anna Levina <anna_levina@internet.ru>
2025-11-26 18:16:35 +0300

    feat: настроено подключение к Google API

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,30 @@
 # app/core/config.py
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
+from pydantic import BaseSettings, EmailStr
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_description: str = 'Проект для бронирования переговорок'
     database_url: str | None = None
     secret: str = 'SECRET'
+
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    # Переменные для Google API
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
+
 
     model_config = SettingsConfigDict(env_file='.env')
 
